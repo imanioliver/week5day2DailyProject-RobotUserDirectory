64302efc4cb539968ac7c86f4d6a55f6920fa534
Imani Oliver <isaac@ihardy.co>
2017-08-16 09:10:53 -0400

    css is showing

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.set('views', './views')
 app.set('view engine', 'mustache')
 
 
+app.use(express.static('./public'));
+
 
 app.get('/', function (req, res) {
   res.render('index', {users: users});
@@ -19,8 +21,18 @@ app.listen(3000, function () {
 })
 
 
+// app.get('/listing/:id', function(req, res){
+//     console.log(req.params.id);
+//     res.render('profile', {users:users[i]});
+//     // res.send("YAY");
+// });
+
+
 app.get('/listing/:id', function(req, res){
+    let user= data.users[req.params.id];
+
+
     console.log(req.params.id);
-    res.render('profile', {users: users});
+    res.render('profile', user);
     // res.send("YAY");
 });
