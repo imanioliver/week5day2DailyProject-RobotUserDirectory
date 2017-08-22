1df2fa9e16ceb55c34a836054fe887e37fa92b07
Imani Oliver <isaac@ihardy.co>
2017-08-22 17:19:45 -0400

    initial commit from rework, week 6 day 2

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,156 @@
 const express = require('express');
-const Data = require ('../models/data.js')
 const router = express.Router();
+// const Data = require ('../models/data.js')
+
+let data =[];
+
+const getListings = function(req, res, next) {
+    const MongoClient = require('mongodb').MongoClient; //reference to the Mongo client
+    const assert = require('assert'); //test to see if we have errors
+
+    const url = 'mongodb://localhost:27017/robots'; //represents our mongo database,
+
+    MongoClient.connect(url, function(err, db) { //we call the client and tell it what error, and what database to connect to, make sure there's no errors
+        assert.equal(null, err); //does the error equal null? if so good! if it's false, it won't even try to connect to the database
+
+        getData(db, function(){ //this is the call back function of the "getData" variable we created below
+            db.close();
+            next(); //sends us to the endpoint
+        });
+    });
+
+    let getData = function (db, callback) { //it calls, needs a database and gives a callback function,
+        let users = db.collection("users"); //sets the variable "user" to the dataabase we have. same as "db.users" that we did earlier
+
+        users.find({}).toArray().then(function(users){ //find all of the users, .toArray returns a promise, THEN set the info i received into a variable call data, then I do my callback function from line 16
+            data = users; //set the 50 objects = to the data object at the top
+            callback();
+        });
+
+    };
+};
+
+const getLooking = function(req, res, next) {
+    const MongoClient = require('mongodb').MongoClient; //reference to the Mongo client
+    const assert = require('assert'); //test to see if we have errors
+
+    const url = 'mongodb://localhost:27017/robots'; //represents our mongo database,
+
+    MongoClient.connect(url, function(err, db) { //we call the client and tell it what error, and what database to connect to, make sure there's no errors
+        assert.equal(null, err); //does the error equal null? if so good! if it's false, it won't even try to connect to the database
+
+        getData(db, function(){ //this is the call back function of the "getData" variable we created below
+            db.close();
+            next(); //sends us to the endpoint
+        });
+    });
+
+    let getData = function (db, callback) { //it calls, needs a database and gives a callback function,
+        let users = db.collection("users"); //sets the variable "user" to the dataabase we have. same as "db.users" that we did earlier
+
+        users.find({"job": null}).toArray().then(function(users){ //find all of the users, .toArray returns a promise, THEN set the info i received into a variable call data, then I do my callback function from line 16
+            data = users; //set the 50 objects = to the data object at the top
+            callback();
+        });
+
+    };
+};
+
+const getEmployed = function(req, res, next) {
+    const MongoClient = require('mongodb').MongoClient; //reference to the Mongo client
+    const assert = require('assert'); //test to see if we have errors
+
+    const url = 'mongodb://localhost:27017/robots'; //represents our mongo database,
+
+    MongoClient.connect(url, function(err, db) { //we call the client and tell it what error, and what database to connect to, make sure there's no errors
+        assert.equal(null, err); //does the error equal null? if so good! if it's false, it won't even try to connect to the database
+
+        getData(db, function(){ //this is the call back function of the "getData" variable we created below
+            db.close();
+            next(); //sends us to the endpoint
+        });
+    });
+
+    let getData = function (db, callback) { //it calls, needs a database and gives a callback function,
+        let users = db.collection("users"); //sets the variable "user" to the dataabase we have. same as "db.users" that we did earlier
+
+        users.find({"job": {$nin: [null]}}).toArray().then(function(users){ //find all of the users, .toArray returns a promise, THEN set the info i received into a variable call data, then I do my callback function from line 16
+            data = users; //set the 50 objects = to the data object at the top
+            callback();
+        });
+
+    };
+};
+
+const getCountry = function(req, res, next) {
+    const MongoClient = require('mongodb').MongoClient; //reference to the Mongo client
+    const assert = require('assert'); //test to see if we have errors
+
+    const url = 'mongodb://localhost:27017/robots'; //represents our mongo database,
+
+    MongoClient.connect(url, function(err, db) { //we call the client and tell it what error, and what database to connect to, make sure there's no errors
+        assert.equal(null, err); //does the error equal null? if so good! if it's false, it won't even try to connect to the database
+
+        getData(db, function(){ //this is the call back function of the "getData" variable we created below
+            db.close();
+            next(); //sends us to the endpoint
+        });
+    });
+
+    let getData = function (db, callback) { //it calls, needs a database and gives a callback function,
+        let users = db.collection("users"); //sets the variable "user" to the dataabase we have. same as "db.users" that we did earlier
+
+        users.find({}).toArray().then(function(users){ //find all of the users, .toArray returns a promise, THEN set the info i received into a variable call data, then I do my callback function from line 16
+
+            data = users; //set the 50 objects = to the data object at the top
+            callback();
+        });
+
+    };
+};
 
 
-
-router.get('/', function (req, res) {
-  res.render('index', {users: Data.allUsers});
+router.get("/", getListings, function(req, res){ //when we call our endpoint, we access "getListings" first then we can render based on the database data.
+    res.render("listing", {users: data})
 });
 
 
+router.get('/looking',getLooking,  function(req, res) {
+    res.render("looking", {users:data})
+});
+
+router.get('/employed', getEmployed, function (req, res) {
+  res.render('employed', {users: data});
+});
+
+
+
+
+
+router.get('/country', getCountry, function(req, res){
+    // let country = req.params.address.country; //req comes from the parent(grab from client), params for when you build with dynamic route, and every time you call params (a property object that exists on the req object), it allows you to call dynamic parts of the object (go the dynamic route with id, or with name etc. )
+    // let user = data.find(function(user){
+    //     return user.address.country==country; //please return any user.id that equals the id that was clicked
+    // })
+    //
+    //
+    // console.log(req.params.address.country);
+    res.render('country', {users:data});
+    // res.send("YAY");
+});
+
 router.get('/listing/:id', function(req, res){
     let id = req.params.id; //req comes from the parent(grab from client), params for when you build with dynamic route, and every time you call params (a property object that exists on the req object), it allows you to call dynamic parts of the object (go the dynamic route with id, or with name etc. )
-
-    let user = Data.allUsers.find(function(user){
+    let user = data.find(function(user){
         return user.id==id; //please return any user.id that equals the id that was clicked
-
-    });
+    })
 
 
     console.log(req.params.id);
     res.render('profile', user);
     // res.send("YAY");
 });
+
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express            = require('express');
 // const users              = require('../data.js');
-//
+const mustache           = require('mustache');
 const mustacheExpress    = require('mustache-express')
-const app                = express();
-const Data               = require('./models/data.js')
+
+// const Data               = require('./models/data.js')
 const routes             = require('./routes/index.js')
+const path               = require("path");
+
+const app                = express();
 
 app.engine('mustache', mustacheExpress());
-app.set('views', './views')
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'mustache')
-app.set("index", "index")
+app.set("layout", "layout")
 
 
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 app.listen(3000, function () {
